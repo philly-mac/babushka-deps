b9adfb5dd1fe7d3fa2000c1c77f2354b8a1f880e
Philip MacIver <philip.maciver@xing.com>
2012-03-28 22:34:55 +0200

    Fixed ruby-build

--- rbfu.rb
+++ rbfu.rb
@@ -2,7 +2,7 @@ dep 'ruby-build' do
   met? { File.exists?('/usr/local/bin/ruby-build') }
   meet do
     cd '/tmp' do
-      log_shell "Fetching ruby-build", "https://github.com/sstephenson/ruby-build.git" if !File.exists?("rbfu") || !File.directory?("ruby-build")
+      log_shell "Fetching ruby-build", "https://github.com/sstephenson/ruby-build.git" if !File.exists?("ruby-build") || !File.directory?("ruby-build")
       cd 'ruby-build' do
         shell './install.sh'
       end
