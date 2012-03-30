af52342d69469b04c2dc2455c6ddc92cf6726ece
Philip MacIver <philip.maciver@xing.com>
2012-03-30 04:40:01 +0200

    Always make syntax errors

--- passenger.rb
+++ passenger.rb
@@ -1,7 +1,7 @@
 dep 'ruby 1.9.3' do
   requires 'rbfu inited'
 
-  met?
+  met? do
     shell "env | grep -i GEM_HOME | grep 'rubies\/1.9.3'"
   end
 
@@ -10,8 +10,6 @@ dep 'ruby 1.9.3' do
   end
 
 end
-
-
 
 dep 'passenger gem', :template => 'gem' do
   requires 'ruby 1.9.3'
