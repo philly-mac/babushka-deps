1537aea6217aeb349bbe83fa15060c0c3f1721d5
Philip MacIver <philip.maciver@xing.com>
2012-04-08 07:12:47 +0200

    rbfu init

--- passenger.rb
+++ passenger.rb
@@ -6,10 +6,10 @@ dep 'passenger gem', :template => 'gem' do
   requires 'ruby 1.9.3', "rbfuify philip bashrc"
 
   met? do
-    File.exists?("/usr/local/rbfu/rubies/1.9.3/bin/passenger")
+    File.exists?("/usr/local/rbfu/rubies/1.9.3/lib/ruby/gems/bin/passenger")
   end
 
   meet do
-    log_shell "Installing passenger", "su - philip -c \"source #{babushka_root}/rbfu/rbfu-init; gem install passenger\""
+    log_shell "Installing passenger", "su - philip -c \"bash -l -c 'rbfu @1.9.3 gem uninstall passenger'\""
   end
 end
