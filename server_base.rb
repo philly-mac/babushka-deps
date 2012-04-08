380e6d1f829799096e7bb37e0eeb82d0eea714bb
Philip MacIver <philip.maciver@xing.com>
2012-04-08 22:08:03 +0200

    pcre

--- server_base.rb
+++ server_base.rb
@@ -5,7 +5,9 @@ end
 dep 'rsync', :template => 'managed'
 dep 'curl', :template => 'managed'
 dep 'grep', :template => 'managed'
-dep 'pcre', :template => 'managed'
+dep 'pcre', :template => 'managed' do
+  provides []
+end
 dep 'postfix', :template => 'managed'
 dep 'htop', :template => 'managed'
 dep 'git', :template => 'managed'
