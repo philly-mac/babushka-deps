117addf126f7e084a9b709d32d0521425e1acbbc
Philip MacIver <philip.maciver@xing.com>
2012-04-08 18:26:28 +0200

    nginx

--- nginx.rb
+++ nginx.rb
@@ -1,5 +1,5 @@
 dep 'nginx' do
-  met? { '/opt/nginx/sbin/nginx'}
+  met? { '/opt/nginx/sbin/nginx'.p.exists? }
 
   meet { shell "rbfu @1.9.3 passenger-install-nginx-module  --auto --auto-download --prefix=/opt/nginx"}
 
