7dafa12a49beae65088f3409dd6292404d1a2c12
Philip MacIver <philip.maciver@xing.com>
2012-04-08 11:16:47 +0200

    auto start services

--- babushka.rb
+++ babushka.rb
@@ -6,11 +6,8 @@ def service_installed?(service)
   path = '/etc/rc.conf'
 
   File.readlines(path).each do |line|
-    puts "reading line #{line}"
     if daemons = /^DAEMONS=\((.*)\)/.match(line)
-      puts "matcth #{daemons.captures} #{daemons}"
-      daemons = daemons[1]
-      return daemons.split(' ').include?(service)
+      return daemons[1].split(' ').include?(service)
     end
   end
   return false
@@ -26,7 +23,7 @@ def mod_service(service, replacement = nil)
 
     if daemons = /^DAEMONS=\((.*)\)/.match(line)
       daemons = daemons[1]
-      if (daemon = /([!@\(\s]#{service}[\s\)])/.match(daemons)) && replacement
+      if (daemon = /([\(\s]#{service}[\s\)])/.match(daemons)) && replacement
         daemon = daemon[1]
         daemons = daemons.sub(daemon, replacement)
         line = "DAEMONS=(#{daemons})"
