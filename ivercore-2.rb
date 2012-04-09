40adb5138a0673c06dd0001e5d140b4be1d05779
Philip MacIver <philip.maciver@xing.com>
2012-04-09 12:21:16 +0200

    Moved elastic saerch to ivercore-2

--- ivercore-2.rb
+++ ivercore-2.rb
@@ -2,6 +2,7 @@ dep 'IverCore-2' do
   requires  'user philip',
     'sudoers',
     'server base',
+    'elasticsearch',
     'sshd configure'.with(:allowed_users => 'philip git')
     'sshd auto start'
 end
--- server_base.rb
+++ server_base.rb
@@ -39,7 +39,6 @@ dep 'server base' do
     'git',
     'java',
     'ruby',
-    'elasticsearch',
     'passenger gem',
     'nginx',
     'iptables config',
