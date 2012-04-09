2988b9d16f81ef80d1be255c7fce2d5afc5b4d90
Philip MacIver <philip.maciver@xing.com>
2012-04-09 07:46:55 +0200

    backup

---
+++ backup.rb
@@ -0,0 +1,33 @@
+dep 'backup cron' do
+  met? { babushka_config?('/etc/con.d/backup') }
+  meet do
+    render_erb 'cron.d/backup.erb', :to => '/etc/cron.d/backup'
+  end
+end
+
+dep 's3cmd' do
+
+  met? do
+    File.exists?("/opt/s3cmd")
+  end
+
+  meet do
+    cd '/opt' do
+      log_shell "Downloading s3cmd", "git clone https://github.com/s3tools/s3cmd.git"
+    end
+  end
+end
+
+dep 'backup' do
+  requires 'rsync', 's3cmd', 'backup cron'
+
+  met? do
+    File.exists?("/opt/backup")
+  end
+
+  meet do
+    cd '/opt' do
+      log_shell "Downloading backup", "git clone git://github.com/philly-mac/backup.git"
+    end
+  end
+end
