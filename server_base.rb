7c51d75542e4c60b536f8a5cec4b9da7dae534db
Philip MacIver <philip@ivercore.com>
2012-04-11 10:30:32 +0200

    Finished off some deps

--- server_base.rb
+++ server_base.rb
@@ -1,19 +1,25 @@
-dep 'java', :template => 'managed' do
-  installs "jre7-openjdk-headless"
-end
-
+dep 'imagemagick', :template => 'managed'
+dep 'libxml2', :template => 'managed'
+dep 'libxslt', :template => 'managed'
+dep 'postgresql', :template => 'managed'
 dep 'rsync', :template => 'managed'
 dep 'tmux', :template => 'managed'
 dep 'curl', :template => 'managed'
 dep 'grep', :template => 'managed'
-dep 'pcre', :template => 'managed' do
-  provides []
-end
 dep 'postfix', :template => 'managed'
 dep 'htop', :template => 'managed'
 dep 'git', :template => 'managed'
+
 dep 'mongodb', :template => 'managed' do
   provides 'mongod'
+end
+
+dep 'pcre', :template => 'managed' do
+  provides []
+end
+
+dep 'java', :template => 'managed' do
+  installs "jre7-openjdk-headless"
 end
 
 dep 'mongodb auto start' do
@@ -21,9 +27,24 @@ dep 'mongodb auto start' do
   meet { mod_service('mongodb') }
 end
 
+dep 'postfix auto start' do
+  requires 'postfix'
+  met? { service_installed?('postfix') }
+  meet { mod_service('postfix') }
+end
+
 dep 'linux-lts', :template => 'managed' do
   provides []
 end
+
+dep 'forward email', :user_dir, :email do
+  path = "#{user_dir}/.forward"
+
+  met? { path.p.exists? }
+  meet { shell "echo #{email} > #{path}" }
+end
+
+root forward
 
 dep 'server base' do
   requires 'linux-lts',
@@ -38,11 +59,16 @@ dep 'server base' do
     'htop',
     'git',
     'java',
+    'imagemagick',
+    'libxml2',
+    'libxslt',
+    'postgresql',
     'ruby',
     'passenger gem',
     'nginx',
     'iptables config',
-    'postfix',
+    'postfix auto start',
+    'forward email'.with(:user_dir => '/root', :email => 'philip@ivercore.com')
     "vim config",
-    'backup'
+    'backup',
 end
