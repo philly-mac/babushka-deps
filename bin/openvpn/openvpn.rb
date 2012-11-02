be2cf73973debd0cc56ef20ee907323ad9e834a7
Philip MacIver <philip@ivercore.com>
2012-11-02 11:01:15 +0100

    Tweaking the openvpn scripts

--- bin/openvpn/openvpn.rb
+++ bin/openvpn/openvpn.rb
@@ -4,8 +4,41 @@ require 'pty'
 require 'expect'
 
 PTY.spawn("./openvpn_1.sh") do |reader, writer, pid|
- result = reader.expect("Country Name")
- writer.puts "DE"
- result = reader.expect("bye")
- p result
+
+  result = reader.expect("Country Name")
+  writer.puts "DE"
+
+  result = reader.expect "State or Province Name"
+  writer.puts "Berlin"
+
+  result = reader.expect "Locality Name"
+  writer.puts "Berlin"
+
+  result = reader.expect "Organization Name"
+  writer.puts "SuitePad"
+
+  result = reader.expect "Organizational Unit Name"
+  writer.puts "IT"
+
+  result = reader.expect "Common Name"
+  writer.puts ""
+
+  result = reader.expect "Name []"
+  writer.puts "IT"
+
+  result = reader.expect "Email Address"
+  writer.puts ""
+
+  result = reader.expect "A challenge password"
+  writer.puts "."
+
+  result = reader.expect "An optional company name"
+  writer.puts "."
+
+  result = reader.expect "Sign the certificate"
+  writer.puts "y"
+
+  result = reader.expect "1 out of 1 certificate requests certified, commit"
+  writer.puts "y"
+
 end
