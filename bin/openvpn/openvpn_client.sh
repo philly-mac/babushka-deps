4653b6c2d31471cead537f6c6678015656687036
Philip MacIver <philip@ivercore.com>
2012-11-02 12:29:04 +0100

    Tweaking the openvpn scripts

--- bin/openvpn/openvpn_client.sh
+++ bin/openvpn/openvpn_client.sh
@@ -14,6 +14,10 @@ cd /etc/openvpn/easy-rsa/2.0/keys
 
 [ ! -e ${OPENVPN_CLIENT_HOME} ] && mkdir ${OPENVPN_CLIENT_HOME}
 
-cp client.key client.crt ca.crt "${KEY_USER}.conf" ${OPENVPN_CLIENT_HOME}
+cp ${KEY_USER}.* ca.crt ${OPENVPN_CLIENT_HOME}
+
+cd /usr/share/doc/openvpn/examples/sample-config-files
+
+cp client.conf ${OPENVPN_CLIENT_HOME}
 
 chown -Rf ${USER}:${USER} ${OPENVPN_CLIENT_HOME}
