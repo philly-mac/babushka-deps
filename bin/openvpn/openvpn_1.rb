7690fbaf396e30078b0aeebaf5e46a919f8bae9a
Philip MacIver <philip@ivercore.com>
2012-11-02 11:07:41 +0100

    Tweaking the openvpn scripts

--- bin/openvpn/openvpn_1.rb
+++ bin/openvpn/openvpn_1.rb
@@ -6,35 +6,35 @@ require 'expect'
 PTY.spawn("./openvpn_1.sh") do |reader, writer, pid|
 
   result = reader.expect("Country Name")
-  puts reader
+  puts result
   writer.puts "DE"
 
   result = reader.expect "State or Province Name"
-  puts reader
+  puts result
   writer.puts "Berlin"
 
   result = reader.expect "Locality Name"
-  puts reader
+  puts result
   writer.puts "Berlin"
 
   result = reader.expect "Organization Name"
-  puts reader
+  puts result
   writer.puts "SuitePad"
 
   result = reader.expect "Organizational Unit Name"
-  puts reader
+  puts result
   writer.puts "IT"
 
   result = reader.expect "Common Name"
-  puts reader
+  puts result
   writer.puts ""
 
   result = reader.expect "Name []"
-  puts reader
+  puts result
   writer.puts "IT"
 
   result = reader.expect "Email Address"
-  puts reader
+  puts result
   writer.puts "developers@suitepad.de"
 
 end
