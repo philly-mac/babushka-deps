a7b4a9a794744d65caa3283b03b277245eeb20ef
Philip MacIver <philip@ivercore.com>
2012-11-29 14:52:06 +0100

    add noraml methods for utils

--- deps/postfix.rb
+++ deps/postfix.rb
@@ -1,7 +1,7 @@
 dep 'postfix.managed' do
 
   before do
-    shell "apt-get -y --purge remove sendmail-base sendmail-cf sendmail-doc; apt-get install -y mailutils; apt-get install --reinstall postfix"
+    shell "apt-get -y --purge remove sendmail-base sendmail-cf sendmail-doc; apt-get install -y mailutils; apt-get install -y --reinstall postfix"
   end
 
 
