79db21f5b59866336c4b464c97b1c5a762c9e8dd
Philip MacIver <philip@ivercore.com>
2012-12-02 05:04:18 +0100

    updated the setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,7 +5,7 @@ if [[ `id -u` != 0 ]]; then
   exit 1
 fi
 
-apt-get -y wget install expect libcurl4-openssl-dev curl git build-essential \
+apt-get -y install wget expect libcurl4-openssl-dev curl git build-essential \
   openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev \
   libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev \
   autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config
