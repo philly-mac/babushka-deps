cd6ab74b60c7f7b37ce9a84aaff7ffd9974ef73d
Philip MacIver <philip@ivercore.com>
2012-12-03 06:00:05 +0100

    Updating dep

--- deps/nginx.rb
+++ deps/nginx.rb
@@ -5,9 +5,10 @@ dep 'nginx from passenger' do
 
   meet do
     cd '/tmp' do
-      if !"/tmp/rails-nginx-passenger-ubuntu".p.exists?
-        shell "git clone git://github.com/jnstq/rails-nginx-passenger-ubuntu.git"
+      if "/tmp/rails-nginx-passenger-ubuntu".p.exists?
+        shell "rm -rf rails-nginx-passenger-ubuntu"
       end
+      shell "git clone git://github.com/jnstq/rails-nginx-passenger-ubuntu.git"
       shell "mv rails-nginx-passenger-ubuntu/nginx/nginx /etc/init.d/nginx"
       shell "chown root:root /etc/init.d/nginx"
     end
